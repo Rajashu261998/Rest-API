238e12bbee250dd03294767e1594bf4085668345
ASHUTOSH RAJ <ashutoshraj0420@gmail.com>
2022-12-02 22:39:43 +0530

    add

--- app.js
+++ app.js
@@ -3,5 +3,9 @@ const express = require("express")
 const app = express()
 
 app.use((req,resp,next)=>{
-    resp.status(200)
+    resp.status(200).json({
+        message: "app is running"
+    })
 })
+
+module.exports = app
